e948e0b822b3015416aed0682e2778df0b349387
TheeNinja <theeninja.development@gmail.com>
2019-07-27 13:03:03 -0700

    Initial commit.

---
+++ src/main/java/org/seifer/gosearch/GoSearch.java
@@ -0,0 +1,11 @@
+package org.seifer.gosearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoSearch {
+    public static void main(final String[] args) {
+        SpringApplication.run(GoSearch.class, args);
+    }
+}
